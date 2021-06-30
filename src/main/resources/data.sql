325276cead4eb8a6a76a303d617ba916d0f63962
bdziadosz96 <72156687+bdziadosz96@users.noreply.github.com>
2021-06-30 17:03:21 +0200

    Create yaml configure file, database loaded from file

--- src/main/resources/data.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE car
-(
-    id    INT AUTO_INCREMENT PRIMARY KEY,
-    name  VARCHAR(250) NOT NULL,
-    model VARCHAR(250) NOT NULL
-);
-
-insert into car(id,name,model) values (1, 'Audi', 'A8');
